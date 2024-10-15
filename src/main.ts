5090122d1eef693aa933e7eff8dc3058c7a2ca7d
alief abdillah <aliefmabdillah09@gmail.com>
2024-10-15 16:48:34 +0700

    feat: integrate with swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookshelf')
+    .setDescription(
+      'Simple Bookshelf API Documentation with NestJS Framework and MongoDB Database',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.use(helmet()); //implement helment
   app.enableCors(); //implement cors
   app.useGlobalPipes(
